6bd6bc6e6bfc1f438e3e36d020373fc3115d175d
farisahamadh <34473943+farisahamadh@users.noreply.github.com>
2020-10-31 20:14:58 +0530

    Update install_pgbackrest.sh

--- setup/install_pgbackrest.sh
+++ setup/install_pgbackrest.sh
@@ -25,12 +25,13 @@ sleep 1
 echo "Retrieving pgbackrest source code from github"
 sleep 2
 wget https://github.com/pgbackrest/pgbackrest/archive/release/2.30.zip
-unzip pgbackrest-release-2.30.zip
+unzip 2.30.zip
 echo "Installing build dependencies"
-apt-get install make gcc libpq-dev libssl-dev libxml2-dev pkg-config liblz4-dev libzstd-dev libbz2-dev libz-dev
-apt-get install postgresql-client libxml2
+apt-get -y install make gcc libpq-dev libssl-dev libxml2-dev pkg-config liblz4-dev libzstd-dev libbz2-dev libz-dev
+apt -y install perl libtime-hires-perl libdigest-sha-perl libjson-pp-perl
+apt-get -y install postgresql-client libxml2
 cd pgbackrest-release-2.30/src && ./configure && make
-cp pgbackrest-release-2.30/src/pgbackrest /usr/bin/pgbackrest
+cp pgbackrest /usr/bin/pgbackrest
 chmod 755 /usr/bin/pgbackrest
 echo "Completed pgbackrest installation"
 sleep 1
